dc97ce59e45fcb55e697d7d780c09e0ef83fbcf0
Bailey Carlson <bcarlson9067@jh-mba158-02.d214.org>
2018-11-06 11:07:14 -0600

    added variable to change image

--- IntergalacticTravel/StarViewController.swift
+++ IntergalacticTravel/StarViewController.swift
@@ -11,22 +11,26 @@ import UIKit
 class StarViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
-    
+    var isRedDwarf: Bool!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if isRedDwarf {
+            imageView.image = UIImage(named: "redDwarfStar")
+        } else {
+            imageView.image = UIImage(named: "blueDwarfStar")
+        }
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
